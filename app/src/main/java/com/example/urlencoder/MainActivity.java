dac1645dc5dc71ffcee07aa72e10c9a9c2e46073
dlihubbi-source <dlihubbi@gmail.com>
2025-07-15 23:07:15 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/example/urlencoder/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.urlencoder;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/url_encoder.html");
+    }
+}
